c8e37d26fc0ae1b3bec360aac1e685d3f0f96407
gdumie01 <22841055+gdumie01@users.noreply.github.com>
2023-11-06 00:24:20 +0000

    Add files via upload

--- train.py
+++ train.py
@@ -15,13 +15,13 @@ dv.fit(train_dict)
 X_train = dv.transform(train_dict)
 
 
-features = dv.get_feature_names_out()
+features = dv.get_feature_names_out().tolist()
 dtrain = xgb.DMatrix(X_train, label=y_train, feature_names=features)
 
 xgb_params = {
-    'eta': 0.1, 
-    'max_depth': 6,
-    'min_child_weight': 1,
+    'eta': 0.9, 
+    'max_depth': 10,
+    'min_child_weight': 10,
     
     'objective': 'binary:logistic',
     'nthread': 8,
@@ -30,7 +30,7 @@ xgb_params = {
     'verbosity': 0,
 }
 
-model = xgb.train(xgb_params, dtrain, num_boost_round=100)
+model = xgb.train(xgb_params, dtrain, num_boost_round=5)
 
 with open('model.bin', 'wb') as f_out: # 'wb' means write-binary
     pickle.dump((dv, model), f_out)
